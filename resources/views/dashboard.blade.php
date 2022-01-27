c94c590ae51fd643cda9909bb5c721bb1671b0b9
raya <rayavexed@gmail.com>
2022-01-27 17:15:01 +0700

    init

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,5 @@
+<x-app-layout>
+   <div class="p-5">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio adipisci accusamus natus quod. Obcaecati cumque optio dolor est accusamus quisquam minus eum ut, dignissimos quia, eaque quibusdam. Fuga laboriosam adipisci ut distinctio suscipit quos placeat incidunt, autem cumque consequatur doloribus eligendi omnis asperiores ea sint, excepturi consequuntur nemo obcaecati esse. Voluptatum molestias enim minima ad laborum maxime, recusandae, consectetur labore dolorum dicta expedita nemo natus quo. Rem, accusamus alias quis earum et illum ab blanditiis dicta fugiat magnam eaque adipisci fugit non unde consectetur voluptatum beatae ducimus provident quibusdam repellendus? Numquam voluptatum consequuntur et necessitatibus eos quisquam, cumque voluptate perspiciatis?
+   </div>
+</x-app-layout>
---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,32 @@
+<div class="w-1/5 min-h-screen bg-gray-900 text-gray-300 p-5">
+    <div class=" mb-5">
+        <a href="#" class="font-semibold text-xl hover:text-gray-50 transition duration-300">Lunacast</a>
+    </div>
+    <div class="mb-5">
+        <div class="p-2">
+            <div class=" uppercase text-gray-400 tracking-wide text-xs py-1">
+                Playlist
+            </div>
+            <a href="#" class="block hover:text-gray-50 py-1">Create</a>
+            <a href="#" class="block hover:text-gray-50 py-1">Index</a>
+        </div>
+    </div>
+    <div class="mb-5">
+        <div class="p-2">
+            <div class=" uppercase text-gray-400 tracking-wide text-xs py-1">
+                Category
+            </div>
+            <a href="#" class="block hover:text-gray-50 py-1">Create</a>
+            <a href="#" class="block hover:text-gray-50 py-1">Index</a>
+        </div>
+    </div>
+    <div class="mb-5">
+        <div class="p-2">
+            <div class=" uppercase text-gray-400 tracking-wide text-xs py-1">
+                Users
+            </div>
+            <a href="#" class="block hover:text-gray-50 py-1">Create</a>
+            <a href="#" class="block hover:text-gray-50 py-1">Index</a>
+        </div>
+    </div>
+</div>
